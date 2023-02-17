11a6656cbba2de9465e82033dae69451b4fc7219
BILLOTTA ELIES <elies.billotta@heckel02.u-pem.fr>
2023-02-17 10:30:53 +0100

    mandelbrot en cours

--- TP1/exo5.cpp
+++ TP1/exo5.cpp
@@ -28,10 +28,29 @@
 using namespace std;
 
 int isMandelbrot(int z[], int n, int point[]){
+    if (n==0){
+        return 0;
+    }
 
+    if (z[0]*z[0]+z[1]*z[1]>4){
+        return n;
+    }
+
+    int z2[2];
+    z2[0]=z[0]*z[0]-z[1]*z[1]+point[0];
+    z2[1]=2*z[0]*z[1]+point[1];
+
+    return isMandelbrot(z2, n-1, point);
 }
 
 int main()
 {
+    int z[2];
+    z[0]=0;
+    z[1]=0;
+    int point[2];
+    point[0]=0;
+    point[1]=0;
+    cout << isMandelbrot(z, 100, point) << endl;
     return 0;
 }
