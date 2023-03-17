2af55311ca9835bad9690e69972f639104b08159
BILLOTTA ELIES <elies.billotta@heckel02.u-pem.fr>
2023-03-17 10:31:26 +0100

    let's go les tas

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -1,7 +1,7 @@
-#include "tp4.h"
-#include "mainwindow.h"
+//#include "tp4.h"
+//#include "mainwindow.h"
 
-#include <QApplication>
+/*#include <QApplication>
 #include <time.h>
 #include <stdio.h>
 
@@ -49,4 +49,66 @@ int main(int argc, char *argv[])
 	w->show();
 
 	return a.exec();
+}*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Heap{
+
+	std::vector<int> data;
+
+	int Heap::leftChildIndex(int nodeIndex){
+		return nodeIndex*2+1;
+	}	
+
+	int Heap::rightChildIndex(int nodeIndex)
+{
+    return nodeIndex*2+2;
+}
+
+	void Heap::insertHeapNode(int heapSize, int value)
+	{
+		// use (*this)[i] or this->data[i] to get a value at index i
+		int i = heapSize;
+		data[i] = value;
+		while(i > 0 && data[i] > data[(i-1)/2]){
+			int temp = data[i];
+			data[i] = data[(i-1)/2];
+			data[(i-1)/2] = temp;
+			i = (i-1)/2;
+		}
+	}
+
+	void Heap::heapify(int heapSize, int nodeIndex)
+	{
+		// use (*this)[i] or this->data[i] to get a value at index i
+		int i = 0;
+		int i_max = nodeIndex;
+		if (i_max != i){
+			int temp = data[i];
+			data[i] = data[i_max];
+			data[i_max] = temp;
+			heapify(heapSize, i_max);
+		}
+
+	}
+
+	void Heap::buildHeap(std::vector<int> array, int numbers)
+	{
+
+	}
+
+	void Heap::heapSort()
+	{
+
+	}
+
+
+};
+
+int main(){
+	return 1;
 }
