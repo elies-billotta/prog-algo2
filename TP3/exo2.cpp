53e2969213377d41462f472378e80bef6d971f8b
elies <elies.billtta@edu.univ-eiffel.fr>
2023-03-19 12:25:20 +0100

    finish binary search all

--- TP3/exo2.cpp
+++ TP3/exo2.cpp
@@ -10,9 +10,24 @@ using namespace std;
  * @param indexMin first index of the value to find
  * @param indexMax last index of the value to find
  */
+
 void binarySearchAll(std::vector<int> array, int toSearch, int& indexMin, int& indexMax)
 {
-	
+	int mid = 0;
+	int start = 0; 
+	int end = array.size();
+	while(start < end){
+		mid = (start + end) / 2;
+		if (toSearch > array[mid]) start = mid + 1;
+		else if (toSearch < array[mid]) end = mid;
+		else {
+			indexMin = mid;
+			indexMax = mid;
+			while (array[indexMin] == toSearch) indexMin--;
+			while (array[indexMax] == toSearch) indexMax++;
+			return;
+		}
+	}
 }
 
 
@@ -20,9 +35,9 @@ void binarySearchAll(std::vector<int> array, int toSearch, int& indexMin, int& i
 int main(int argc, char *argv[])
 {
 	std::vector<int> array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int toSearch = 10;
+	int toSearch = 1;
 	int indexMin = 0;
-	int indexMax = 0;
+	int indexMax = 7;
 	binarySearchAll(array, toSearch, indexMin, indexMax);
 	cout << "Le chiffre " << toSearch << " est à l'index : " << indexMin << " et " << indexMax << endl;
 	return 0;
